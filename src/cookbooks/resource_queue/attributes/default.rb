450370a8bfa50df8ec0668518989d3cb13469a0c
Petrik van der Velde <petrikvandervelde@gmail.com>
2018-04-23 22:23:05 +1200

    Updating to the latest version of rabbitmq

--- src/cookbooks/resource_queue/attributes/default.rb
+++ src/cookbooks/resource_queue/attributes/default.rb
@@ -36,7 +36,7 @@ default['firewall']['ipv6_enabled'] = false
 # RABBITMQ
 #
 
-rabbitmq_version = '3.7.3'
+rabbitmq_version = '3.7.4'
 default['rabbitmq']['version'] = rabbitmq_version
 
 # For some reason the rabbitmq cookbook doesn't do the right thing, eventhough it should
